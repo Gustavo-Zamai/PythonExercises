f23c2dc8795d0237196c9c1582c1fd30267e642f
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-03-14 14:43:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Ex01
+print("Alo, Mundo!")
+###############################################
+#Ex02
+num = input("Digite um número: ")
+print("O número digitado foi: ",num)
+################################################
+#Ex03
